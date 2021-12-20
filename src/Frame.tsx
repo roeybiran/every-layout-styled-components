7e9a807b1757cc36814a8638ad42818624f7f249
roeybiran <37002381+roeybiran@users.noreply.github.com>
2021-12-20 11:45:14 +0200

    fix: bug in Frame

--- src/Frame.tsx
+++ src/Frame.tsx
@@ -7,30 +7,30 @@ type FrameProps = {
 
 /** A custom element for augmenting image ratios */
 export const Frame = styled.div<FrameProps>`
-  display: block;
-  position: relative;
+	display: block;
+	position: relative;
 
 	--ratio: ${(p) =>
 		p
 			.ratio!.split(':')
 			.map(Number)
-			.reduce((a, b) => b / a)}
-  padding-block-end: calc(var(--ratio) * 100%);
+			.reduce((a, b) => b / a)};
+	padding-block-end: calc(var(--ratio) * 100%);
 
-  > * {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    inset: 0;
-  }
+	> * {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		inset: 0;
+	}
 
-  > img,
-  > video {
-    block-size: 100%;
-    inline-size: 100%;
-    object-fit: cover;
-  }
+	> img,
+	> video {
+		block-size: 100%;
+		inline-size: 100%;
+		object-fit: cover;
+	}
 `;
 
 Frame.defaultProps = {
